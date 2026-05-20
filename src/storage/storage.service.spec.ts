7b5273027116ce0f60f459b421085754638cacd9
Yovani Navas <yovaninavas10@gmail.com>
2026-05-20 15:38:22 -0400

    errores de npm run test

--- src/storage/storage.service.spec.ts
+++ src/storage/storage.service.spec.ts
@@ -3,13 +3,25 @@ import { StorageService } from './storage.service';
 
 describe('StorageService', () => {
   let service: StorageService;
+  const originalEnv = process.env;
 
   beforeEach(async () => {
+    process.env = {
+      ...originalEnv,
+      GCP_PROJECT_ID: 'test-project',
+      GCP_KEY_FILE_PATH: './test-key.json',
+      GCP_STORAGE_BUCKET_NAME: 'test-bucket',
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [StorageService],
     }).compile();
 
     service = module.get<StorageService>(StorageService);
+  });
+
+  afterEach(() => {
+    process.env = originalEnv;
   });
 
   it('should be defined', () => {
